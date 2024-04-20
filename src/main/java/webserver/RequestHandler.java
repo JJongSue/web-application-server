73c296085dc0b64bbb490bdfab6f81aea13d8db5
json_p <json.p@kakaoenterprise.com>
2024-04-20 17:44:01 +0900

    feature:요구사항1 file 미재존재시 404 에러 반환하게 수정

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -24,17 +24,30 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
             String request = bufferedReader.readLine();
-            log.debug(request);
+            log.debug("request", request);
 
             String[] requests = request.split(" ");
             byte[] body = "Hello World".getBytes();
+            DataOutputStream dos = new DataOutputStream(out);
+
             if(requests.length >= 2) {
                 if(!requests[1].equals("/")) {
                     String filePath = rootPath + requests[1];
+                    File file = new File(filePath);
+
+                    if (!file.exists()) {
+                        log.error("File not found: " + filePath);
+                        String errorMessage = "File not found";
+                        byte[] errorBody = errorMessage.getBytes();
+                        dos = new DataOutputStream(out);
+                        response404Header(dos, errorBody.length);
+                        responseBody(dos, errorBody);
+                        return;
+                    }
                     body = Files.readAllBytes(new File(filePath).toPath());
                 }
             }
-            DataOutputStream dos = new DataOutputStream(out);
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
@@ -46,6 +59,17 @@ public class RequestHandler extends Thread {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response404Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
+            dos.writeBytes("Content-Type: text/plain\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
