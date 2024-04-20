1df4d3458d8c9222b1c4121c4b85d066e78e078d
json_p <json.p@kakaoenterprise.com>
2024-04-20 17:17:14 +0900

    feature:요구사항1 index.html 응답

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,15 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String rootPath = "webapp";
 
     private Socket connection;
 
@@ -23,9 +22,17 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+            String request = bufferedReader.readLine();
+            log.debug(request);
+
+            String[] requests = request.split(" ");
             byte[] body = "Hello World".getBytes();
+            if(requests.length >= 2) {
+                String filePath = rootPath + requests[1];
+                body = Files.readAllBytes(new File(filePath).toPath());
+            }
+            DataOutputStream dos = new DataOutputStream(out);
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
